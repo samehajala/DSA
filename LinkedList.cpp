11627e0e9ceb55fb437723730eeffd2bea67c419
samehajala <118663582+samehajala@users.noreply.github.com>
2023-07-09 11:05:35 +0100

    DeleteNodePosition

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -47,14 +47,59 @@ void insertAfter(Node* prev,int newData)
     prev->link=newNode ; 
     return ; 
 }
+void deleteNode(Node **head ,int pos) 
+{
+    if(*head==nullptr)
+    {
+        return ; 
+    }
+    Node* temp=*head ; 
+    if (pos==1)
+    {
+        *head=temp->link ; 
+        delete(temp) ; 
+    }
+    Node* current=nullptr ; 
+    int i=1 ; 
+    while (i<pos && temp!=nullptr)
+    {
+        i++ ; 
+        current=temp ; 
+        temp=temp->link ; 
+    }
+    current->link=temp->link ; 
+    delete(temp ) ; 
+    return ; 
+    
+}
+void printLinkedList(Node* head)
+{
+    if(head==nullptr)
+    { 
+        cout<<"Empty"<<endl ; 
+        return ; 
+    }
+    while(head!=nullptr)
+    {
+        cout<<head->data<<endl ; 
+        head=head->link ; 
+    }
+    return ; 
+}
 int main()
 {
     Node* A = nullptr ; 
-    for (int i = 0; i < 10; i++)
-    {
-    Append(&A,i) ;  
-    }
-    push(&A,20) ; 
-    cout<<A->data<<endl ; 
+    push(&A,1) ; 
+    push(&A,2) ; 
+    push(&A,3) ; 
+    push(&A,4) ; 
+    push(&A,5) ; 
+    push(&A,6) ; 
+    push(&A,7) ; 
+    push(&A,8) ;
+    push(&A,9) ; 
+    printLinkedList(A) ; 
+    deleteNode(&A,5) ; 
+    printLinkedList(A) ; 
     return 0 ; 
 }
