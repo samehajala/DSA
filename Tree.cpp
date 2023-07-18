9115e1b36d90f0a2e8dc8bf6417c34698f12ba91
samehajala <118663582+samehajala@users.noreply.github.com>
2023-07-18 12:30:31 +0100

    Print Level Order Technique

--- Tree.cpp
+++ Tree.cpp
@@ -43,7 +43,45 @@ void printPostOrder(Node* node)
     printPostOrder(node->right) ; 
     cout<<node->data ; 
 }
-
+// Print Level Order Technique
+int height(Node* node)
+{
+    if(node==nullptr)
+    {
+        return 0 ; 
+    }
+    int lheight=height(node->left)  ; 
+    int rheight=height(node->right) ; 
+    if (lheight>rheight)
+    {
+        return(lheight+1) ; 
+    }
+    else
+    {
+        return rheight+1 ; 
+    }
+    
+}
+void printCurrentLevel(Node* root, int level)
+{
+    if (root==nullptr)
+    {
+        return ; 
+    }
+    if (level==1)
+    {
+        cout<<root->data<<endl ; 
+    }
+    printCurrentLevel(root->left,level-1) ;
+    printCurrentLevel(root->right,level-1) ;  
+}
+void printLevelOrder(Node* root)
+{
+    int h = height(root);
+    int i;
+    for (i = 1; i <= h; i++)
+        printCurrentLevel(root, i);
+}
 int main()
 {
     
