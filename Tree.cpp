d8176ff894a7c4cda2b753137637d7ed717e2e55
samehajala <118663582+samehajala@users.noreply.github.com>
2023-07-18 11:27:26 +0100

    Tree Traversal Techniques

--- Tree.cpp
+++ Tree.cpp
@@ -12,6 +12,37 @@ public:
         right=nullptr ; 
     }
 };
+// Tree Traversal Techniques
+void printInorder(Node* node){
+    if (node==nullptr)
+    {
+        return ; 
+    }
+    printInorder(node->left) ; 
+    cout<<node->data ; 
+    printInorder(node->right)  ; 
+
+}  
+void printPreOrder(Node* node)
+{
+    if (node==nullptr)
+    {
+        return ; 
+    }
+    cout<<node->data ; 
+    printPreOrder(node->left) ; 
+    printPreOrder(node->right) ;  
+}
+void printPostOrder(Node* node)
+{
+    if(node==nullptr)
+    {
+        return ; 
+    }
+    printPostOrder(node->left) ; 
+    printPostOrder(node->right) ; 
+    cout<<node->data ; 
+}
 
 int main()
 {
