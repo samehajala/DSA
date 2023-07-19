6ac9f7a2f3c1a4b2d94517c5258aae12013b1fa1
samehajala <118663582+samehajala@users.noreply.github.com>
2023-07-19 16:48:26 +0100

    Level order traversal  in Spiral form

--- Tree.cpp
+++ Tree.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue> 
+#include<stack>
 using namespace std ; 
 class Node {
 public:
@@ -116,8 +117,58 @@ Node* insertNode(Node* root,int data )
     }
     
 }
+//Level order traversal of Binary Tree in Spiral form : 
+void printSpiralOrder(Node *root)
+{
+    if(root==nullptr)
+    {
+        return ; 
+    }
+    queue<Node*> q ; 
+    stack<Node*> s ; 
+    bool leftToRight=true ;
+    q.push(root) ;  
+    for (int i = 0; i <q.size() ; i++)
+    {
+        Node* current=q.front() ; 
+        q.pop() ; 
+        if(leftToRight)
+        {
+           cout<<current->data<<"\t" ; 
+        }
+        else
+        {
+            s.push(current) ; 
+        }
+        if(current->left)
+        {
+            q.push(current->left) ; 
+        }
+        if(current->right)
+        {
+            q.push(current->right) ; 
+        }
+        if (!leftToRight)
+        {
+            while (!s.empty())
+            {
+                Node *temp=s.top() ; 
+                cout<<temp->data<<"\t" ; 
+            }
+        }
+        leftToRight=!leftToRight ;    
+    }
+}
+
 int main()
 {
-    
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(7);
+    root->left->right = new Node(6);
+    root->right->left = new Node(5);
+    root->right->right = new Node(4);
+    printCurrentLevel(root,3) ; 
     return 0 ; 
 }
